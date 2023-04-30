69ecd31fbcdd7c54fe09071db0d2b8b4e75b5ccf
Danton Matheus <37675973+danton03@users.noreply.github.com>
2023-04-29 22:03:19 -0300

    feat: cria as models das entidades do projeto

---
+++ src/main/java/edu/danton/padroesdeprojetosspring/model/EnderecoRepository.java
@@ -0,0 +1,7 @@
+package edu.danton.padroesdeprojetosspring.model;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface EnderecoRepository extends CrudRepository<Endereco, String> {
+
+}
