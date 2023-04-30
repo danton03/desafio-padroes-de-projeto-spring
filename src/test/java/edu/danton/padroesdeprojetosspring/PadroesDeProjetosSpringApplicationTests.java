e2dcc8e24365542bc38b138b7c5f3f8e81000844
Danton Matheus <37675973+danton03@users.noreply.github.com>
2023-04-29 21:53:25 -0300

    inicia o projeto

---
+++ src/test/java/edu/danton/padroesdeprojetosspring/PadroesDeProjetosSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.danton.padroesdeprojetosspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PadroesDeProjetosSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
